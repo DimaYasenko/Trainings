6956febda4664eb1b56520e18a0738c6c898ad44
DimaYasenko <dima.yasenko@gmail.com>
2017-08-05 16:02:36 +0100

    Remove datascript-chard folder

--- Clojurescript/web-development-in-clojure/appendix-1/src/appendix_1/core.clj
+++ Clojurescript/web-development-in-clojure/appendix-1/src/appendix_1/core.clj
@@ -1,5 +1,22 @@
 (ns appendix-1.core
   (:gen-class))
 
+;;; data types
+(list 1 2 3)
+'(1 2 3)
+[1 2 3]
+(vec '(1 2 3))
+(vector 1 2 3)
+#{1 2 3}
+{:first 1 :second 2}
+
+;;; anonymous function
+((fn [arg] (println arg)) "hello")
+
+(#(println %1 %2 %3) "Hello" "from" "Dima!")
+
+;;; named functions
+
+
 (defn -main [& args]
   (println "Appendix 1: Clojure Primer (not done yet)"))
